5105c9fa52a083a3f4d17feaeba8345f326a4dce
EzeBernardine <demayous2@gmail.com>
2018-08-20 12:03:26 +0100

    validated signin and signup forms

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,23 +3,38 @@ import {BrowserRouter, Route, Switch,Link , NavLink} from 'react-router-dom';
 import AllHeader from './AllHeader';
 import Button from './Button'
 
+
 export default class SignIn extends React.Component{
-  handleSignin=(e)=>{
-    e.preventDefault()
-    alert('signin');
+  state={
+    signinError: undefined
   }
+  handleSigninform=(e)=>{
+    e.preventDefault()
+    let name = document.forms[0].signinName.value.trim()
+    let password = document.forms[0].signinPassword.value.trim()
+
+    let nameList = localStorage.getItem('nameList');
+    let confirmpasswordList = localStorage.getItem('confirmpasswordList');
+    if(nameList.includes(name)){
+      if(confirmpasswordList.includes(password)){
+        this.setState(()=>({signinError: 'Success'}))
+      }else{this.setState(()=>({signinError: 'failed'}))}
+      }else{this.setState(()=>({signinError: 'failed'}))}
+      
+    }
   render(){
     return(
       <div className='signIn'>
-       <form >
+       <form onChange={this.handleChange} onSubmit={this.handleSigninform}>
         <div className='signIn_container'>
           <div className = 'signIn_name' >
+            <h3 className='signin_error'>{this.state.signinError}</h3>
             <h3>Name</h3>
-            <input></input>
+            <input required={true} name="signinName" id='signinName'></input>
           </div>
           <div className = 'signIn_passward' >
             <h3>Passward</h3>
-            <input></input>
+            <input name="signinPassword" required={true} id='signinPassword'></input>
           </div>
         <Button signin={this.handleSignin}/>
         </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -10,49 +10,45 @@ let userNameList=[]
 export default class SignUp extends React.Component{
   state={
     usernameError: undefined,
-    emptyUsername: undefined,
-    emptyPassword: undefined,
     confirmpasswordError: undefined,
     emailError: undefined
   };
     handleSignupSubmit = (e) => {
       e.preventDefault()
-      let p = [',', '.', '/', '<', '>', '?', ':', ';', '"', '', '[', '{', ']', '}', '\|', '|', '\'', '-', '_', '=', '+', ')', '(', '*', '&', '^', '%', '$', '#', '@', '!', '~', '`']
+
       let name = document.forms[0].name.value.trim()
       let password = document.forms[0].password.value.trim()
       let confirmpassword = document.forms[0].confirmpassword.value.trim()
       let username = document.forms[0].username.value.trim()
       let email = document.forms[0].email.value.trim()
-      nameList.push(name)
-
-      {!email.includes('@') && email.length>0? (this.setState(()=>({emailError: 'email must contain @ character'}))) : (this.setState(()=>({emailError: undefined})))}
-      {email.length <=0? (this.setState(()=>({emailError: 'email is required'}))) : (this.setState(()=>({emailError: undefined})))}
-      if(email.length>0 && !emailList.includes(email) && email.includes('@')){
-        emailList.push(email);
-        console.log(emailList)
-      }
-// let seen = p.forEach((i)=>i)
-// console.log(seen)
-// console.log((p.forEach((i) => p[i])))
-      {!(confirmpassword === password) ? (this.setState(()=>({confirmpasswordError: 'password does not match'}))) : (this.setState(()=>({confirmpasswordError: undefined})))}
-      for(let i =0; i<=p.length; i++){
-        {( p[i]=== password)? (this.setState(()=>({emptyPassword: 'put a valid password'}))) : (this.setState(()=>({emptyPassword: undefined})))}
-      }
-
-      if(!confirmpasswordList.includes(confirmpassword) && confirmpassword.length>0 && (confirmpassword === password)){
-        confirmpasswordList.push(confirmpassword)
-        console.log(confirmpasswordList)
-      }
+      let seen = /[a-zA-Z0-9_]{8,12}/;
       
 
-      { userNameList.includes(username) ?(this.setState(()=>({usernameError: 'usename  already exist'}))) :(this.setState(()=>({usernameError: undefined})))}
-      {!username.length > 0?(this.setState(()=>({emptyUsername: 'input a valid username'}))) :(this.setState(()=>({emptyUsername: undefined})))}
-      if (!userNameList.includes(username) && username.length > 0) {
-        userNameList.push(username)
-        console.log(userNameList)
+      { userNameList.includes(username) ? (this.setState(() => ({ usernameError: 'usename  already exist' }))) : (this.setState(() => ({ usernameError: undefined }))) }
+
+      if(email.includes('@')){
+        if((confirmpassword === password) && password.match(seen)){ 
+          if (!userNameList.includes(username)) {
+            userNameList.push(username)
+            emailList.push(email);
+            confirmpasswordList.push(confirmpassword);
+            nameList.push(name)
+            window.location.href = '/Signin'
+          }
+        }
       }
+
+      {!email.includes('@')? (this.setState(()=>({emailError: 'email must contain @ character'}))) : (this.setState(()=>({emailError: undefined})))}
+      
+      {!(confirmpassword === password) ? (this.setState(()=>({confirmpasswordError: 'password does not match'}))) : (this.setState(()=>({confirmpasswordError: undefined})))}
+
+      {!password.match(seen)  ? (this.setState(()=>({emptyPassword: 'your password should be alphabate and numbers, $ btween 8-12'}))) : (this.setState(()=>({emptyPassword: undefined})))}
+
+      localStorage.setItem('nameList', nameList)
+      localStorage.setItem('userNameList', userNameList)
+      localStorage.setItem('confirmpasswordList', confirmpasswordList)
+      localStorage.setItem('emailList', emailList)
     }
- 
   render(){
     return(
       <div className='signUp'>
@@ -62,32 +58,31 @@ export default class SignUp extends React.Component{
          </div>
          <div className='signUp_name'>
            <h3>NAME</h3>
-           <input id='name'></input>
+            <input required={true} id='name'></input>
          </div>
          <div className='signUp_username'>
            <h3>USER NAME 
-             <span className='signUp_error_empty'>{this.state.usernameError}{this.state.emptyUsername}</span>
+             <span className='signUp_error_empty'>{this.state.usernameError}</span>
            </h3>
-           <input id='username'></input>
+            <input required={true} id='username'></input>
          </div>
          <div className='signUp_passward'>
            <h3>PASSWORD
-             <span className='signUp_error_empty'>{this.state.emptyPassword}</span>
            </h3>
-           <input id='password'></input>
+            <input required={true} id='password'></input>
          </div>
          <div className='signUp_email'>
            <h3>EMAIL
               <span className='signUp_error_empty'>{this.state.emailError}</span>
            </h3>
-           <input id='email'></input>
+           <input required={true} id='email'></input>
          </div>
          <div className='signUp_confirmpassward'>
             <h3>CONFIRM PASSWORD
               <span className='signUp_error_empty'>{this.state.confirmpasswordError}</span>
             </h3>
-           <input id='confirmpassword'></input>
-         < Button signup={this.handleSignup}/ >
+            <input required={true} id='confirmpassword'></input>
+         < Button / >
          </div>
        </form>
        
