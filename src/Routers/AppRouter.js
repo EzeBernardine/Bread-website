12ce1887b02530a2096c55140078911751f36a1c
EzeBernardine <demayous2@gmail.com>
2018-08-24 05:52:24 +0100

    added the cart icn to the subSales conponent

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -4,8 +4,6 @@ import Order from '../components/Order';
 import Home from '../components/Home';
 import Profile from '../components/Profile';
 import NotFound from '../components/NotFound';
-// import subProfile from '../components/SubProfile';
-// import subSales from '../components/SubSales';
 import SignUp from '../components/SignUp';
 import SignIn from '../components/SignIn';
 import Top from '../components/Top'
@@ -24,8 +22,6 @@ const AppRouter = () => (
             <Route path="/order" component={Order} exact={true} />
             <Route path="/Profile" component={Profile }/>
             <Route path="/sigUp" component={SignUp }/>
-            {/* <Route path="/subProfile" component={subProfile }/>
-            <Route path="/subSales" component={subSales }/> */}
             <Route component={NotFound} />
       </Switch>
     </div>
--- src/components/SubSales.js
+++ src/components/SubSales.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {FaCartArrowDown} from 'react-icons/fa'
+import { NavLink} from 'react-router-dom';
 
 export default class SubSales extends React.Component{
   render(){
@@ -12,6 +14,9 @@ export default class SubSales extends React.Component{
           < div className = 'subProfile_phone' >
             <h2>Current sales</h2>
             {<h3>active</h3>}
+          </div>
+          <div className='subSales_cart'>
+            <NavLink activeClassName="subSales_cart" to="/order/"><FaCartArrowDown /></NavLink>
           </div>
         </div>
       </div>
